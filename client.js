5c801e4a1cc2bcb96136f0f59a098d3716d34a34
Camilo Gomez <camilogomezmusic@gmail.com>
2020-07-29 00:37:53 +0000

    refactored code to add a separate module for the TCP connection, client.js'

---
+++ client.js
@@ -0,0 +1,24 @@
+const net = require('net');
+/**
+ * Establishes connection with the game server
+ */
+const connect = function() {
+  const conn = net.createConnection({
+    host: 'localhost',
+    port: 50541
+  });
+
+  //interpret data as text
+  conn.setEncoding('utf8');
+
+  //event handler for incoming data
+  conn.on('data', (data) => {
+    console.log(data);
+  })
+
+  return conn;
+};
+
+module.exports = {
+  connect
+};
--- play.js
+++ play.js
@@ -1,25 +1,4 @@
-const net = require('net');
-
-
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
-  });
-
-  //interpret data as text
-  conn.setEncoding('utf8');
-
-  //event handler for incoming data
-  conn.on('data', (data) => {
-    console.log(data);
-  })
-
-  return conn;
-};
+const { connect } = require('./client');
 
 console.log('Connecting...');
 connect();
