04e2f1d8d19b07d9ce661c6aa3535395825f724b
Camilo Gomez <camilogomezmusic@gmail.com>
2020-07-29 13:42:23 +0000

    Added an input.js module to handle the client keyboard input setup

---
+++ input.js
@@ -0,0 +1,40 @@
+const readline = require('readline');
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function() {
+  const stdin = process.stdin;
+  readline.emitKeypressEvents(stdin); //this, to emit the key events
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  //ask about this tomorrow. Im not registering it as "data".
+  stdin.on('keypress', (str, key) => handleUserInput(key) );
+
+  return stdin;
+};
+
+const handleUserInput = function(data) {
+  if (data && data.ctrl && data.name === 'c') process.exit();
+  switch(data.name) {
+    case 'a':
+      console.log(data.name);
+      return "Move: left";
+    case 's':
+      console.log(data.name);
+      return 'Move: down';
+    case 'd':
+      console.log(data.name);
+      return "Move: right";
+    case 'w':
+      console.log(data.name);
+      return "Move: up";
+  }
+};
+
+
+module.exports = {
+  setupInput
+}
--- play.js
+++ play.js
@@ -1,42 +1,8 @@
-const readline = require('readline');
+
 const { connect } = require('./client');
+const { setupInput } = require('./input');
 
 console.log('Connecting...');
 connect();
 
-/**
- * Setup User Interface 
- * Specifically, so that we can handle user input via stdin
- */
-const setupInput = function() {
-  const stdin = process.stdin;
-  readline.emitKeypressEvents(stdin); //this, to emit the key events
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-
-  //ask about this tomorrow. Im not registering it as "data".
-  stdin.on('keypress', (str, key) => handleUserInput(key) );
-
-  return stdin;
-};
-
 setupInput();
-
-const handleUserInput = function(data) {
-  if (data && data.ctrl && data.name === 'c') process.exit();
-  switch(data.name) {
-    case 'a':
-      console.log(data.name);
-      return "Move: left";
-    case 's':
-      console.log(data.name);
-      return 'Move: down';
-    case 'd':
-      console.log(data.name);
-      return "Move: right";
-    case 'w':
-      console.log(data.name);
-      return "Move: up";
-  }
-};
